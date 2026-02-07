793d5fd5b6bc39ab5908558dc635a18ad2c912a4
Ria Makaraju <riamakaraju@gmail.com>
2026-02-07 13:09:01 -0500

    added app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,35 +2,15 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UserSetup from "./pages/user-setup";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       <div className="flex pb-5 flex-col items-center justify-center">
-          <Button>Shadcn Button</Button>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  function App() {
+  return <UserSetup />;
+}
 }
 
 export default App
