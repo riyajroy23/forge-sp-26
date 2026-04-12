d8e8f3657957c1a0a5c7364ddb10145280f53d7e
EmilyF10 <ferry.em@northeastern.edu>
2026-04-12 13:48:29 -0400

    DEV-26: nav and side bar (#23)
    * nav and side bar
    
    * implemented company message page and replies page
    
    * fix: merge changes for demo
    
    * fix: update UI
    
    * feat: update dashboard widgets
    
    * style: remove extra comments
    
    ---------
    
    Co-authored-by: Maia Gonzalez <gonzalez.mai@northeastern.edu>
    Co-authored-by: riyajroy23 <riyajroy23@gmail.com>

---
+++ frontend/src/components/ui/ReplyCard.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Heart, BarChart2, ThumbsUp, MessageSquareText } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { type Response } from "@/Pages/company_msg_board";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
+import CompanyPost from "@/Pages/company_post";
+
+export default function ReplyCard({ response }: { response: Response }) {
+  const [followed, setFollowed] = useState(false);
+
+  return (
+    <Card className="flex flex-row items-start gap-3 px-10 py-11 rounded-lg border border-gray-200 shadow-none">
+      <div className="w-6 h-6 rounded bg-gray-300 shrink-0 flex items-center justify-center">
+        <BarChart2 className="w-4 h-4 text-gray-500 shrink-0 flex items-start justify-start" />
+      </div>
+
+      <div className="flex-1 min-w-0">
+        <div className="container">
+          <p className="font-semibold text-lg text-black font-[var(--font-spacegrotesk)] truncate">
+            {response.user}
+          </p>
+          <p className="text-sm text-gray-500">{response.time}</p>
+        </div>
+
+        <div className=" text-base text-black font-[var(--font-spacegrotesk)]">
+          <p style={{ marginBottom: "20px" }}>{response.answer}</p>
+        </div>
+      </div>
+    </Card>
+  );
+}
