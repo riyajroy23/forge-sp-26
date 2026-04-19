a278d9a88a311fbf263d836c2dc5a09d911f2107
Jolin Yang <yang.jol@northeastern.edu>
2026-04-19 16:30:00 -0400

    add supabase import

--- backend/routes/groupDashboardAndLandingPageEndpoints.js
+++ backend/routes/groupDashboardAndLandingPageEndpoints.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const supabase = require('./supabase');
+
 
 // Return all public groups
 // GET /groups/public - return all public groups
