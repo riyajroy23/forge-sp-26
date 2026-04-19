6515cf47a65e219719b9e99294cea9b6f53b89ef
Jolin Yang <yang.jol@northeastern.edu>
2026-04-19 17:04:32 -0400

    fix imports and connect routes to db

--- backend/routes/browseCompanyEndpoints.js
+++ backend/routes/browseCompanyEndpoints.js
@@ -1,55 +1,56 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
+import { supabase } from '../lib/supabaseClient.js';
 
 // Mock data -- using hard-coded values, waiting for database connection
-const companies = [
-    {
-        company_id: 1,
-        name: 'Microsoft',
-        industry: 'Technology',
-        description: 'Developing and supporting software, services, devices, and solutions',
-        careers_page_url: 'https://careers.microsoft.com/v2/global/en/home.html',
-        logo_url: null,
-        headquarters_location: 'Redmond, WA',
-        roles: [
-            { role_id: 1, title: 'Software Engineer Co-op', area: 'Engineering', relevant_majors: ['Computer Science'] },
-            { role_id: 2, title: 'UI/UX Design Co-op', area: 'Design', relevant_majors: ['Design', 'Psychology', 'Computer Science'] },
-            { role_id: 3, title: 'Product Management Co-op', area: 'Business', relevant_majors: ['Business', 'Computer Science']}
-          ],
-        created_at: new Date('2025-03-15'),
-        updated_at: new Date('2025-03-21')
-    },
-    {
-        company_id: 2,
-        name: 'Boston Consulting Group',
-        industry: 'Consulting',
-        description: 'Global management consulting firm',
-        careers_page_url: 'https://careers.bcg.com/global/en/',
-        logo_url: null,
-        headquarters_location: 'Boston, Massachusetts',
-        roles: [
-            { role_id: 1, title: 'Accounting Co-op', area: 'Business', relevant_majors: ['Accounting', 'Finance'] },
-            { role_id: 2, title: 'Management Consultant Co-op', area: 'Business', relevant_majors: ['Economics', 'Accounting', 'Finance'] }
-          ],
-        created_at: new Date('2025-01-10'),
-        updated_at: new Date('2025-01-27')
-    },
-    {
-        company_id: 3,
-        name: 'Insulet Corporation',
-        industry: 'Healthcare',
-        description: 'Medical device company that develops, manufactures, and sells the Omnipod Insulin Management System',
-        careers_page_url: 'https://insulet.wd5.myworkdayjobs.com/insuletcareers',
-        logo_url: null,
-        headquarters_location: 'Acton, Massachusetts',
-        roles: [
-            { role_id: 1, title: 'Electrical Engineering Co-op', area: 'Engineering', relevant_majors: ['Electrical Engineering'] },
-            { role_id: 2, title: 'Marketing Co-op', area: 'Marketing', relevant_majors: ['Marketing', 'Communications'] }
-          ],
-        created_at: new Date('2025-09-13'),
-        updated_at: new Date('2025-09-25')
-    }
-]
+// const companies = [
+//     {
+//         company_id: 1,
+//         name: 'Microsoft',
+//         industry: 'Technology',
+//         description: 'Developing and supporting software, services, devices, and solutions',
+//         careers_page_url: 'https://careers.microsoft.com/v2/global/en/home.html',
+//         logo_url: null,
+//         headquarters_location: 'Redmond, WA',
+//         roles: [
+//             { role_id: 1, title: 'Software Engineer Co-op', area: 'Engineering', relevant_majors: ['Computer Science'] },
+//             { role_id: 2, title: 'UI/UX Design Co-op', area: 'Design', relevant_majors: ['Design', 'Psychology', 'Computer Science'] },
+//             { role_id: 3, title: 'Product Management Co-op', area: 'Business', relevant_majors: ['Business', 'Computer Science']}
+//           ],
+//         created_at: new Date('2025-03-15'),
+//         updated_at: new Date('2025-03-21')
+//     },
+//     {
+//         company_id: 2,
+//         name: 'Boston Consulting Group',
+//         industry: 'Consulting',
+//         description: 'Global management consulting firm',
+//         careers_page_url: 'https://careers.bcg.com/global/en/',
+//         logo_url: null,
+//         headquarters_location: 'Boston, Massachusetts',
+//         roles: [
+//             { role_id: 1, title: 'Accounting Co-op', area: 'Business', relevant_majors: ['Accounting', 'Finance'] },
+//             { role_id: 2, title: 'Management Consultant Co-op', area: 'Business', relevant_majors: ['Economics', 'Accounting', 'Finance'] }
+//           ],
+//         created_at: new Date('2025-01-10'),
+//         updated_at: new Date('2025-01-27')
+//     },
+//     {
+//         company_id: 3,
+//         name: 'Insulet Corporation',
+//         industry: 'Healthcare',
+//         description: 'Medical device company that develops, manufactures, and sells the Omnipod Insulin Management System',
+//         careers_page_url: 'https://insulet.wd5.myworkdayjobs.com/insuletcareers',
+//         logo_url: null,
+//         headquarters_location: 'Acton, Massachusetts',
+//         roles: [
+//             { role_id: 1, title: 'Electrical Engineering Co-op', area: 'Engineering', relevant_majors: ['Electrical Engineering'] },
+//             { role_id: 2, title: 'Marketing Co-op', area: 'Marketing', relevant_majors: ['Marketing', 'Communications'] }
+//           ],
+//         created_at: new Date('2025-09-13'),
+//         updated_at: new Date('2025-09-25')
+//     }
+// ]
 
 
 // company endpoints
@@ -62,58 +63,57 @@ router.get('/companies', (req, res) => {
 
 // Return the company with the specified name.
 // GET /companies/name/:companyName - return the company with the name. 
-router.get('/companies/name/:companyName', (req, res) => {
+router.get('/companies/name/:companyName', async (req, res) => {
 
-    // extract field
-    const companyName = req.params.companyName;
+    const { data, error } = await supabase
+        .from('Company')
+        .select('*');
 
-    const company = companies.find(c => c.name.toLowerCase() === companyName.toLowerCase());
-
-    // handles case for where company does not exist on the platform
-    if (!company) {
-        return res.status(404).json("Company not found.");
+    if (error || !data?.length) {
+        return res.status(404).json("No companies found.");
     }
 
     // return company (if found)
-    return res.status(200).json(company);
+    return res.status(200).json(data);
 })
 
 
 // Return the companies that match the given industry. 
 // GET /companies/search/industry/:industry - return companies within the specified industry
-router.get('/companies/search/industry/:industry', (req, res) => {
+router.get('/companies/search/industry/:industry', async (req, res) => {
 
-    // extract field
-    const industry = req.params.industry;
+    const { industry } = req.params;
 
-    const matchedCompanies = companies.filter(c => c.industry.toLowerCase() === industry.toLowerCase());
+    const { data, error } = await supabase
+        .from('Company')
+        .select('*')
+        .ilike('industry', industry);
 
-    // handles case for where there aren't any companies in the industry
-    if (matchedCompanies.length === 0) {
+    if (error || !data?.length) {
         return res.status(404).json("No companies found in this industry.");
     }
-
     // return list of companies in the particular industry
-    return res.status(200).json(matchedCompanies);
+    return res.status(200).json(data);
 })
 
 
 // Return the companies with headquarters in the specified location.
 // GET /companies/search/location/:headquarters_location - return companies headquartered in the specific location
-router.get('/companies/search/location/:headquarters_location', (req, res) => {
+router.get('/companies/search/location/:headquarters_location', async (req, res) => {
 
-    // extract field
-    const hq = req.params.headquarters_location;
+    const { headquarters_location } = req.params;
 
-    const matchedCompanies = companies.filter(c => c.headquarters_location.toLowerCase() === hq.toLowerCase());
+    const { data, error } = await supabase
+        .from('Company')
+        .select('*')
+        .ilike('headquarters_location', headquarters_location);
 
-    // handles case for where there aren't any companies headquartered in the specific location
-    if (matchedCompanies.length === 0) {
+    if (error || !data?.length) {
         return res.status(404).json("No companies headquartered in this location.");
     }
 
     // return list of companies with headquartered in that location
-    return res.status(200).json(matchedCompanies);
+    return res.status(200).json(data);
 })
 
 
@@ -121,34 +121,41 @@ router.get('/companies/search/location/:headquarters_location', (req, res) => {
 // GET /companies/search/major/:major - return companies with roles relevant to this major
 router.get('/companies/search/major/:major', (req, res) => {
 
-    // extract field
-    const major = req.params.major;
+    const { major } = req.params;
 
-    const relevantCompanies = companies.filter(c => c.roles.some(r => r.relevant_majors.some(m => m.toLowerCase() === major.toLowerCase())));
+    const { data, error } = await supabase
+        .from('Company')
+        .select('*, roles(*)')
+        .contains('roles.relevant_majors', [major]);
 
-    // handles case for where there aren't any companies with relevant roles
-    if (relevantCompanies.length === 0) {
+    if (error || !data?.length) {
         return res.status(404).json("No companies have roles relevant to this major.");
     }
 
     // return list of companies with relevant roles
-    return res.status(200).json(relevantCompanies);
+    return res.status(200).json(data);
 })
 
 // Return the company with the specified companyId.
 // GET /companies/:companyId - return the company that matches the companyId. 
 router.get('/companies/:companyId', (req, res) => {
 
-    // extract field
-    const companyId = parseInt(req.params.companyId);
+    const companyId = parseInt(req.params.companyId, 10);
 
-    const company = companies.find(c => c.company_id === companyId);
+    if (isNaN(companyId)) {
+        return res.status(400).json("Invalid company ID.");
+    }
 
-    // handles case for where company does not exist on the platform
-    if (!company) {
+    const { data, error } = await supabase
+        .from('Company')
+        .select('*')
+        .eq('id', companyId)
+        .single();
+
+    if (error || !data) {
         return res.status(404).json("Company not found.");
     }
 
     // return company (if found)
-    return res.status(200).json(company);
+    return res.status(200).json(data);
 })
--- backend/routes/companyPeopleInterviewQuestionEndpoints.js
+++ backend/routes/companyPeopleInterviewQuestionEndpoints.js
@@ -1,33 +1,10 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express';
 import { supabase } from '../lib/supabaseClient.js';
 
-// Mock data for company people -- using hard-coded values, waiting for database connection
-// const company_people = [
-//     {
-//         person_id: 1,
-//         name: 'John Smith',
-//         company_id: 1,
-//         role: 'Software Engineer', 
-//         company_email: 'john.smith@google.com',
-//         linkedin_url: null,
-//         created_at: new Date('2025-03-15'),
-//         updated_at: new Date('2025-03-21')
-//     },
-//     {
-//         person_id: 2,
-//         name: 'Jane Williams',
-//         company_id: 2,
-//         role: 'Business Analyst', 
-//         company_email: 'jane.williams@goldmansachs.com',
-//         linkedin_url: null,
-//         created_at: new Date('2025-04-03'),
-//         updated_at: new Date('2025-04-05')
-//     }
-// ]
+const router = express.Router();
+
 
 // company people endpoints
-
 
 // Return the person at the specified company with the specified personId. 
 // GET /companies/:companyId/people/:personId - return the person at the company with that personId
@@ -53,6 +30,9 @@ router.get('/companies/:companyId/people/:personId', async(req, res) => {
             return res.status(200).json(data);
 });
 
+
+
+// company questions endpoints
 
 // Return all people at a specific company, optionally filtered by their role.  
 // GET /companies/:companyId/people?role=... - return all people at the company (filtered by role if provided)
@@ -83,29 +63,28 @@ router.get('/companies/:companyId/people', async(req, res) => {
 });
 
 
-// Mock data for for company interview questions -- using hard-coded values, waiting for database connection
-// const company_interview_questions = [
-//     {
-//         question_id: 1,
-//         question: 'Write an algorithm for binary search.',
-//         type: 'technical',  
-//         company_id: 1,
-//         role: 'Software Engineer Co-op',
-//         created_at: new Date('2026-03-01')
-//     }, 
-//     {
-//         question_id: 2,
-//         question: 'Tell me about a time you dealt with a challenge.',
-//         type: 'behavioural',   
-//         company_id: 2,
-//         role: null, // null means applies to all roles
-//         created_at: new Date('2025-03-17')
-//     }
-// ]
-
-
 // company interview question endpoints
+// Return all behavioural interview questions at the specific company.
+// GET /companies/:companyId/questions/behavioural - return all behavioural interview questions at that company (regardless of role)
+router.get('/companies/:companyId/questions/behaviourial', async(req, res) => {
 
+    // extract field
+    const companyId = parseInt(req.params.companyId);
+
+    const { data, error } =  await supabase        
+            .from('company_interview_questions')
+                .select('*')
+                .eq('company_id', companyId)
+                .eq('type', 'behavioural')
+
+    // handles case for where there are no behaviourial interview questions for company
+    if (error || !data?.length) {
+        return res.status(404).json("No behaviourial interview questions found for this company.");
+    }
+
+    // return all behaviourial interview questions for company (if found)
+    return res.status(200).json(data);
+})
 
 // Return the interview question with the specified questionId. 
 // GET /companies/:companyId/questions/:questionId - return the question with that questionId
@@ -130,28 +109,6 @@ router.get('/companies/:companyId/questions/:questionId', async(req, res) => {
             // otherwise, return question if successful
             return res.status(200).json(data);
 });
-
-// Return all behavioural interview questions at the specific company.
-// GET /companies/:companyId/questions/behavioural - return all behavioural interview questions at that company (regardless of role)
-router.get('/companies/:companyId/questions/behavorial', async(req, res) => {
-
-    // extract field
-    const companyId = parseInt(req.params.companyId);
-
-    const { data, error } =  await supabase        
-            .from('company_interview_questions')
-                .select('*')
-                .eq('company_id', companyId)
-                .eq('type', 'behavioural')
-
-    // handles case for where there are no behavorial interview questions for company
-    if (error || !data?.length) {
-        return res.status(404).json("No behavorial interview questions found for this company.");
-    }
-
-    // return all behavorial interview questions for company (if found)
-    return res.status(200).json(data);
-})
 
 
 // Return all interview questions at the specific company, optionally filtered by role. 
@@ -182,3 +139,5 @@ router.get('/companies/:companyId/questions', async(req, res) => {
     return res.status(200).json(data);
 
 })
+
+export default router;
